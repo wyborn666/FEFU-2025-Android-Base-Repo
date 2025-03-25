b81bf810cee3c69bc51766aa9e9b885732690932
wyborn666 <29ser@list.ru>
2025-03-25 23:16:54 +1000

    third lab added

--- app/src/main/java/co/feip/fefu2025/MainActivity.kt
+++ app/src/main/java/co/feip/fefu2025/MainActivity.kt
@@ -3,51 +3,22 @@ package co.feip.fefu2025
 import android.graphics.Color
 import android.os.Bundle
 import android.widget.Button
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-
+import co.feip.fefu2025.ui.theme.FEFU2025AndroidBaseRepoTheme
 import java.math.RoundingMode
 import kotlin.random.Random
 
-class MainActivity : AppCompatActivity() {
-
-    private lateinit var flexBoxLayout: CustomFlexBoxLayout
-    private lateinit var addButton: Button
-
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
-
-        flexBoxLayout = findViewById(R.id.flexBoxLayout)
-        addButton = findViewById(R.id.addButton)
-
-        addButton.setOnClickListener {
-            addLanguageView()
+        enableEdgeToEdge()
+        setContent {
+            FEFU2025AndroidBaseRepoTheme {
+                RepositoryCard()
+            }
         }
-    }
-
-    private fun addLanguageView() {
-        val randomLanguage = getRandomLanguage()
-
-        val languageView = LanguageView(this)
-
-        val randomColor = Color.argb(
-            255,
-            Random.nextInt(256),
-            Random.nextInt(256),
-            Random.nextInt(256)
-        )
-
-        val randomPercentage = (Random.nextFloat() * 100).toBigDecimal().setScale(1, RoundingMode.HALF_UP).toFloat()
-
-        languageView.setLanguageName(randomLanguage)
-        languageView.setPercentage(randomPercentage)
-        languageView.setCircleColor(randomColor)
-
-        flexBoxLayout.addView(languageView)
-    }
-
-    private fun getRandomLanguage(): String {
-        val languages = Constants.languages
-        return languages.random()
     }
 }
